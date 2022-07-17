a2b3e2b8260a0903b8484c49abb61a69c5740fe6
xamsix <bramkrekels@gmail.com>
2022-07-17 21:00:47 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const AnimatedImage = ({
+  image,
+  alt,
+  startAnimation,
+  width,
+  height,
+  loadingComplete,
+  initialDelay,
+  easeDuration,
+}) => {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: width,
+        height: height,
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: startAnimation ? height : "0",
+          position: "absolute",
+          left: "0",
+          bottom: "0",
+          overflow: "hidden",
+          transition: `all ${
+            easeDuration ? easeDuration : "0.6"
+          }s cubic-bezier(0.5, 1, 0.89, 1) ${
+            initialDelay ? initialDelay : "0"
+          }s`,
+        }}
+      >
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            position: "absolute",
+            bottom: "0",
+            left: "0",
+          }}
+        >
+          <img
+            style={{ width: width, height: height }}
+            src={image}
+            alt={alt}
+            onLoad={loadingComplete}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedImage;
